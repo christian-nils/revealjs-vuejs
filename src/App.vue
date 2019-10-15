b84c5239be4caef44ab777dac445cc989f46d51b
christian-nils <christian-nils.boda@chalmers.se>
2019-10-15 13:06:56 +0200

    update: App.vue and add: revealjs lib

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="reveal">
+    <div class="slides">
+      <section></section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Reveal from "reveal.js/js/reveal";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    //HelloWorld
+  },
+  mounted() {
+    Reveal.initialize();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+@import url("../node_modules/reveal.js/css/reset.css");
+@import url("../node_modules/reveal.js/css/reveal.css");
+@import url("../node_modules/reveal.js/css/theme/blood.css");
+
+/*#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
